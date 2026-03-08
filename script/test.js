e6ad7146c1183055458739e5f905898640430395
Ananya Chakraborty Gourab <aborty174@gmail.com>
2026-03-08 10:19:54 +0600

    interactive section done

--- script/test.js
+++ script/test.js
@@ -1,1 +1,25 @@
-console.log("test functionality coming");
+//console.log("test functionality coming");
+
+document.getElementById("sign-btn").addEventListener("click", function(event) {
+     event.preventDefault(); // stop form reload
+
+    const nameInput = document.getElementById("username");
+    const contactName = nameInput.value;
+
+    console.log(contactName);
+
+    const password=document.getElementById("password");
+    const pin=password.value;
+    console.log(pin);
+
+    if(contactName=="admin" && pin=="admin123"){
+        alert("signin successful");
+
+        window.location.replace("/home.html");
+
+    }else{
+        alert("signin failed");
+        return;
+    }
+
+});
